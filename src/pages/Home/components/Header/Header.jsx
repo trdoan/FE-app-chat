31707fa1d4263141b355e6bfe486dfa8a942fe89
trdoan <tranduongdoan0922@gmail.com>
2022-04-07 16:04:27 +0700

    backup code

--- src/pages/Home/components/Header/Header.jsx
+++ src/pages/Home/components/Header/Header.jsx
@@ -1,35 +1,34 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import VideoCameraBackIcon from '@mui/icons-material/VideoCameraBack';
-import {Link} from "react-router-dom"
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-           <VideoCameraBackIcon/>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <VideoCameraBackIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-           Google Meet
+            Google Meet
           </Typography>
           <Button color="inherit">
-					<Link to="/login" style={{
-						color:"white",
-						textDecoration:"none"
-					}}>Đăng nhập</Link>
-					</Button>
+            <Link
+              to="/login"
+              style={{
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Đăng nhập
+            </Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
